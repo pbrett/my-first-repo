6356bd55317f2eba0872961882c4c195eb98d4df
pbrett <pbrett_11@yahoo.co.uk>
2019-07-28 10:47:52 +0100

    Added a function and a couple of comments

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # My Project
-Sun 28 Jul 10:32:23 BST 2019
+Sun 28 Jul 10:47:05 BST 2019
 - Number of lines contained:
--    18 guessinggame.sh
+-    28 guessinggame.sh
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-number_of_files=$(ls | wc -l)
-guessed=0
+# Function: guess_loop
+# Accepts one parameter - the actual number to guess
+function guess_loop {
 
-while [ $guessed -eq 0  ]; do
-    echo -n "How many files do you think are in this directory? "
-    read guess
+    # Initialise guess to a number which cannot terminate the loop
+    guess=0
+    while [ $guess -ne $1  ]; do
+        echo -n "How many files do you think are in this directory? "
+        read guess
 
-    if [ $guess -eq $number_of_files ]; then
-        guessed=1
-        echo Well done. There are $guess files in this directory.
-    elif [ $guess -lt $number_of_files ]; then
-        echo There are more than $guess files. Try again
-    else
-        echo There are fewer than $guess files. Try again
-    fi
-done
+        if [ $guess -eq $1 ]; then
+            guessed=1
+            echo Well done. There are $guess files in this directory.
+
+        elif [ $guess -lt $1 ]; then
+            echo There are more than $guess files. Try again
+
+        else
+            echo There are fewer than $guess files. Try again
+
+        fi
+    done
+}
+
+# Call guess_loop with the number of files in the current directory.
+guess_loop $(ls | wc -l)
